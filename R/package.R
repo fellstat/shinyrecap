f0a6d2dacf0e3c782cfed7f2dca0d9860ba447b6
ifellows <ianfellows@athena.local>
2026-07-07 20:58:39 -0700

    cran changes

--- R/package.R
+++ R/package.R
@@ -1,8 +1,10 @@
 #' @title Shiny User Interface for Multiple Source Capture Recapture Models
 #' @description Implements user interfaces for log-linear models, bayesian model averaging and bayesian dirichlet process mixture models.
-#' @docType package
 #' @name shinyrecap-package
 #' @author Ian E. Fellows \email{ian@fellstat.com}
 #' @importFrom stats qnorm rnorm runif uniroot
 #' @importFrom Rcapture closedpCI.t
+#' @importFrom future future
+#' @importFrom magrittr use_series
+#' @import HDInterval LCMCR coda dga ggplot2 ipc knitr promises reshape rhandsontable rmarkdown shinycssloaders shinyjs testthat
 NULL
